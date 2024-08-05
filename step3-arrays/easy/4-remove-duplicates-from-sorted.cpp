8d370e7db21a08d8e040c26d7a1995a64cf13fd3
mohitdhatrak <msdhatrak25@gmail.com>
2024-08-05 08:19:39 +0530

    add 1st four array easy

---
+++ step3-arrays/easy/4-remove-duplicates-from-sorted.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+// time complexity = O(n)
+// space complexity = O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int index = 0;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[index]) {
+                index++;
+                nums[index] = nums[i];
+            }
+        }
+
+        return index + 1;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    cout << s.removeDuplicates(nums);
+    cout << endl;
+}
